3cc5eb8c1338710e3ed8dfb2438bc4dcb386dea6
Santiago Rodriguez <santiago.rodriguezmartin@cbtw.tech>
2026-08-02 22:59:32 -0500

    feat: update header component layout and styles for improved navigation

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,13 +1,17 @@
-import { Component, input } from '@angular/core'
+import { Location } from '@angular/common'
+import { Component, inject, input } from '@angular/core'
 import { RouterLink } from '@angular/router'
+import { ChevronLeftIcon } from '../icons/chevron-left-icon'
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, ChevronLeftIcon],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
+  private readonly location = inject(Location)
+
   title = input('StudyFlash')
   subTitle = input<string>()
 }
