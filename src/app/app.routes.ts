06f748bdd3869de48f4e7549b9430b776dd0bd77
Santiago Rodriguez <santiago.rodriguezmartin@cbtw.tech>
2026-08-03 21:36:47 -0500

    feat: update routing to redirect to home page and adjust header link

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,28 +1,27 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
 
-const loadAdminPage = () => import('./pages/admin.page/admin.page').then((m) => m.AdminPage);
+const loadAdminPage = () => import('./pages/admin.page/admin.page').then((m) => m.AdminPage)
 
 export const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: 'home'
+    redirectTo: '',
   },
   {
-    path: 'home',
-    loadComponent: () => import('./pages/home.page/home.page').then((m) => m.HomePage)
+    path: '',
+    loadComponent: () => import('./pages/home.page/home.page').then((m) => m.HomePage),
   },
   {
     path: 'admin',
-    loadComponent: loadAdminPage
+    loadComponent: loadAdminPage,
   },
   {
     path: 'study/:slug',
-    loadComponent: () =>
-      import('./pages/study-flash.page/study-flash.page').then((m) => m.StudyFlashPage)
+    loadComponent: () => import('./pages/study-flash.page/study-flash.page').then((m) => m.StudyFlashPage),
   },
   {
     path: '**',
-    redirectTo: 'home'
-  }
-];
+    redirectTo: '',
+  },
+]
